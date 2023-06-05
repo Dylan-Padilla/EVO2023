1fdb497e95ee5bee22eb1c91558dee136b58a4bb
Dylan-Padilla <dylan.padilla94@gmail.com>
2023-06-05 15:10:44 -0700

    updtes

--- flowchart.R
+++ flowchart.R
@@ -30,9 +30,9 @@ DiagrammeR::grViz("digraph {
 graph [layout = dot, rankdir = LR, fontcolor = orange, bgcolor=black]
 
 # define the global styles of the nodes. We can override these in box if we wish
-node [shape = rectangle, style = filled, fillcolor = gray, color = orange, fontname = Courier, height=10, fontsize = 50]
+node [shape = rectangle, style = filled, fillcolor = gray, color = orange, fontname = Courier, height=15, fontsize = 70]
 
-data1 [label = 'An analysis of genetic differentiation based on Fst estimates revealed two major genetic clusters; one with all the American samples grouped together, \n and the other one grouping the European samples. Similarly, there is some degree of genetic differentiation among samples within each continent, \n suggesting a total of 6 ancestral populations k with unique patterns of allelic variation in the for gene. The results indicate that \n different patterns of gene flow as expected under models of isolation by environment (IBE) and isolation by distance (IBD) \n are likely driving the genetic differentiation among populations of D. melanogaster.', shape = invhouse, fillcolor = Beige, fontcolor = purple]
+data1 [label = 'An analysis of genetic differentiation based on Fst estimates revealed two major genetic clusters; one with all the American samples grouped together, and the other one grouping \n the European samples. This analysis suggests a total of 6 ancestral populations with unique patterns of allelic variation at the for gene. The results also indicate that \n different patterns of gene flow as expected under models of isolation by environment and isolation by distance \n are likely driving the genetic differentiation among populations of D. melanogaster.', shape = invhouse, fillcolor = Beige, fontcolor = purple]
 
 
 # edge definitions with the node IDs
